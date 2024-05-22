64e61fce64e895f5465c0d28762d59c738b5dcb9
Kevin Hernández Escalante <kuhes0918@gmail.com>
2024-05-22 14:24:48 -0600

    3 implements votes

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
-from .models import Question
+from urllib import request
+from django.shortcuts import redirect, render
+from .models import Choice, Question
 
 # Create your views here.
 def home(request):
     questions = Question.objects.all()
 
+    
     return render(request, 'polls/home.html', {
         "questions": questions
     })
 
 def vote(request, q_id):
-    pass
+    question = Question.objects.get(id=q_id)
+    if request.method == 'POST':
+        choice = request.POST['choice']
+        c = Choice.objects.get(id=choice)
+        c.votes +=1
+        c.save()
+        return redirect('polls:results', q_id)
 
-def results(reuqest, q_id):
-    pass
+    return render(request, 'polls/question.html',
+                  {"question":question })
+
+def results(request, q_id):
+    question = Question.objects.get(id=q_id) 
+    return render(request, 'polls/results.html',
+                  {"question": question })
